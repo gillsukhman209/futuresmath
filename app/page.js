e3397e69f741ee334d8ef47f45eb5e08f5a3de86
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2024-08-28 15:55:45 -0700

    removed account drawdown

--- app/page.js
+++ app/page.js
@@ -53,26 +53,6 @@ export default function Home() {
     }
   };
 
-  const calculateAccountLife = (maxDrawDown, riskPerTrade, maxTradesPerDay) => {
-    if (riskPerTrade === 0 || maxTradesPerDay === 0) {
-      return "Cannot calculate account life. Risk per trade and max trades per day must be greater than zero.";
-    }
-    const totalRiskPerDay = riskPerTrade * maxTradesPerDay;
-    const accountLifeDays = Math.round(maxDrawDown / totalRiskPerDay);
-    return `Your account can withstand approximately ${accountLifeDays} days or ${
-      maxTradesPerDay * accountLifeDays
-    }   maximum losses before reaching the maximum drawdown.`;
-  };
-
-  const handleCalculateAccountLife = () => {
-    const message = calculateAccountLife(
-      maxDrawDown,
-      riskPerTrade,
-      maxTradesPerDay
-    );
-    setAccountLifeMessage(message);
-  };
-
   const calculateTradeSize = () => {
     if (!product || riskPerTrade <= 0 || stopLoss <= 0) {
       setTradeSizeResult("Please enter valid values for all fields.");
@@ -119,11 +99,11 @@ export default function Home() {
 
   return (
     <ThemeProvider attribute="class">
-      <div className="min-h-screen w-full bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 text-white flex flex-col items-center justify-center p-4 md:p-8 transition-all duration-300">
+      <div className="min-h-screen  w-full bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500 dark:from-gray-900 dark:via-gray-800 dark:to-gray-700 text-white flex flex-col items-center justify-center p-4 md:p-8 transition-all duration-300">
         <h1 className="text-5xl md:text-6xl font-extrabold mb-12 text-center bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 via-red-400 to-pink-400 dark:from-blue-300 dark:via-indigo-400 dark:to-purple-500 animate-pulse">
           Futures Math
         </h1>
-        <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 w-full max-w-6xl">
+        <div className="flex justify-center  md:flex-row space-y-8 md:space-y-0 md:space-x-8 w-full max-w-6xl bg">
           <motion.div
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
@@ -207,86 +187,6 @@ export default function Home() {
               </motion.p>
             )}
           </motion.div>
-
-          <motion.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-            className="bg-white bg-opacity-10 dark:bg-gray-800 dark:bg-opacity-30 backdrop-blur-2xl rounded-3xl p-6 md:p-10 shadow-2xl w-full md:w-1/3 border border-white border-opacity-20 dark:border-gray-700"
-          >
-            <h2 className="text-3xl md:text-4xl font-extrabold mb-6 md:mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 dark:from-blue-400 dark:via-indigo-500 dark:to-purple-600">
-              Account Life Calculator
-            </h2>
-            <div className="space-y-6 md:space-y-8">
-              <div>
-                <label
-                  htmlFor="maxDrawDown"
-                  className="block text-sm font-medium mb-2 text-pink-200 dark:text-blue-200"
-                >
-                  Maximum Drawdown
-                </label>
-                <input
-                  id="maxDrawDown"
-                  type="number"
-                  onChange={(e) => setMaxDrawDown(Number(e.target.value))}
-                  placeholder="Enter amount"
-                  className="w-full p-3 md:p-4 bg-white bg-opacity-10 dark:bg-gray-700 dark:bg-opacity-50 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300 dark:focus:ring-blue-400 transition text-white placeholder-gray-300 dark:placeholder-gray-400"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="riskPerTrade"
-                  className="block text-sm font-medium mb-2 text-pink-200 dark:text-blue-200"
-                >
-                  Risk Per Trade
-                </label>
-                <input
-                  id="riskPerTrade"
-                  type="number"
-                  onChange={(e) => setRiskPerTrade(Number(e.target.value))}
-                  placeholder="Enter amount"
-                  className="w-full p-3 md:p-4 bg-white bg-opacity-10 dark:bg-gray-700 dark:bg-opacity-50 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300 dark:focus:ring-blue-400 transition text-white placeholder-gray-300 dark:placeholder-gray-400"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="maxTradesPerDay"
-                  className="block text-sm font-medium mb-2 text-pink-200 dark:text-blue-200"
-                >
-                  Max Trades in a Day
-                </label>
-                <input
-                  id="maxTradesPerDay"
-                  type="number"
-                  onChange={(e) => setMaxTradesPerDay(Number(e.target.value))}
-                  placeholder="Enter number of trades"
-                  className="w-full p-3 md:p-4 bg-white bg-opacity-10 dark:bg-gray-700 dark:bg-opacity-50 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-300 dark:focus:ring-blue-400 transition text-white placeholder-gray-300 dark:placeholder-gray-400"
-                />
-              </div>
-
-              <motion.button
-                whileHover={{
-                  scale: 1.05,
-                  boxShadow: "0 0 15px rgba(255,255,255,0.5)",
-                }}
-                whileTap={{ scale: 0.95 }}
-                onClick={handleCalculateAccountLife}
-                className="w-full bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 dark:from-blue-500 dark:via-indigo-600 dark:to-purple-700 text-white font-bold py-3 md:py-4 px-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1"
-              >
-                Calculate Account Life
-              </motion.button>
-            </div>
-            {accountLifeMessage && (
-              <motion.p
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-                className="mt-6 md:mt-8 text-base md:text-lg font-semibold text-center bg-white bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-30 p-4 md:p-6 rounded-xl border border-white border-opacity-20 dark:border-gray-600 shadow-inner"
-              >
-                {accountLifeMessage}
-              </motion.p>
-            )}
-          </motion.div>
         </div>
         <Toaster />
         <div className="fixed bottom-0 left-0 w-full overflow-hidden bg-black bg-opacity-50 py-2">
@@ -294,7 +194,7 @@ export default function Home() {
             className="whitespace-nowrap text-white font-bold text-sm md:text-lg"
             style={{
               transform: `translateX(${bannerPosition}%)`,
-              transition: "transform 5s linear",
+              transition: "transform 1s linear",
             }}
           >
             🎉 Use my affiliate code "fearless" for a sweet 5% discount on any
